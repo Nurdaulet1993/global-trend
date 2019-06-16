c49ec3b9e9b9f2de3d0c4438b22d81b26baebebc
Nurdaulet <nurik93sat@gmail.com>
2019-06-16 23:10:17 +0600

    some blocks

---
+++ src/js/hero.js
@@ -0,0 +1,9 @@
+let windowWidth = $(window).width();
+
+$('.hero').mousemove(function(event){
+    let moveX = (($(window).width() / 2) - event.pageX) * 0.1;
+    let moveY = (($(window).height() / 2) - event.pageY) * 0.1;
+
+    $('.hero .animate').css('margin-left', moveX + 'px');
+    $('.hero .animate').css('margin-bottom', moveY + 'px');
+})
