f6e8e59b7fa999a312727119a1f0e240cf14e689
Nurdaulet <nurik93sat@gmail.com>
2019-06-18 02:28:00 +0600

    error on testimonial page

---
+++ src/js/testimonial.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $(".testimonial__slide").owlCarousel({
+        nav: false,
+        dots: false,
+        arrows: false,
+
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    });
+});
