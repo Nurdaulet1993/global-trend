49455247624335064abf5614c93a6db1b73a048f
Nurdaulet <nurik93sat@gmail.com>
2019-06-22 15:57:33 +0600

    modal window+

---
+++ src/js/modal-window.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.modal-group__input').forEach(item => {
+    item.addEventListener('focus', function() {
+        this.parentNode.classList.add('active');
+    })
+
+    item.onblur = function() {
+        this.parentNode.classList.remove('active');
+    }
+
+    item.onpaste = function() {
+        this.value.style.fontSize="14px";
+        this.value.style.height="14px";
+        this.value.style.color="red";
+    }
+})
