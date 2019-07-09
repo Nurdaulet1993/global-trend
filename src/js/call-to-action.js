c7dd9eb480287e97e2a94d39986f66d040850339
Nurdaulet1993 <nurik93sat@gmail.com>
2019-07-09 18:02:57 +0600

    123

---
+++ src/js/call-to-action.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.call-to-action form').forEach(item => {
+    item.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const name = this.querySelector('input[name="name"]').value;
+        const email = this.querySelector('input[name="email"]').value;
+        const phone = this.querySelector('input[name="phone"]').value;
+        
+        if(name.length == 0 || email.length || phone.length) {
+            const warning = document.createElement('div');
+            warning.className = "warning";
+            warning.innerText = "Вы не заполнили все поля!";
+            this.appendChild(warning);
+            setTimeout(function() {
+                document.querySelector('.warning').style.display="none";
+            }, 2000);
+        }else {
+            this.setAttribute('action', './mail.php');
+            this.setAttribute('method', 'POST');
+            this.submit();
+        }
+    })
+})
